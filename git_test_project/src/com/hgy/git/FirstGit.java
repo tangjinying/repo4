7291bd61840ef4d8725936944193a51557072952
tangjinying <2831902836@qq.com>
2020-08-21 17:34:27 +0800

    提交git_test_project

---
+++ git_test_project/src/com/hgy/git/FirstGit.java
@@ -0,0 +1,14 @@
+package com.hgy.git;
+
+public class FirstGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
